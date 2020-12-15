f3bc9dcd1032952be4ac2c0dc52559059818a8ac
Matti Paksula <matti.paksula@iki.fi>
2020-12-15 11:40:13 +0200

    handle disconnects

--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/matti/x11vncfixer
 
 go 1.15
 
-require (
-	9fans.net/go v0.0.2 // indirect
-	github.com/matti/betterio v0.1.0 // indirect
-)
+require github.com/matti/betterio v0.2.1
--- main.go
+++ main.go
@@ -6,11 +6,14 @@ import (
 	"net"
 	"os"
 	"strings"
+	"time"
 
 	"github.com/matti/betterio"
 )
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+
 	protocolVersion := os.Args[1]
 	upstreamAddress := os.Args[2]
 
@@ -43,6 +46,33 @@ func main() {
 
 func handle(conn net.Conn, protocolVersion string, upstreamAddress string) {
 	defer conn.Close()
+
+	var upstream net.Conn
+	retryPrinted := false
+
+	for {
+		if err := betterio.CheckReaderOpen(conn); err != nil {
+			log.Println(conn.RemoteAddr().String(), "client gone or sends garbage", err)
+			return
+		}
+
+		dialer := net.Dialer{
+			Timeout: 100 * time.Millisecond,
+		}
+		var err error
+		upstream, err = dialer.Dial("tcp", upstreamAddress)
+		if err != nil {
+			if !retryPrinted {
+				retryPrinted = true
+				log.Println(conn.RemoteAddr().String(), "dial err to", upstreamAddress, "will retry until client disconnects", err)
+			}
+			time.Sleep(100 * time.Millisecond)
+			continue
+		}
+		break
+	}
+	defer upstream.Close()
+
 	clientProtocolVersionChan := make(chan string, 1)
 	go func() {
 		log.Println(conn.RemoteAddr().String(), "sending protocolVersion", protocolVersion, "to client")
@@ -54,21 +84,21 @@ func handle(conn net.Conn, protocolVersion string, upstreamAddress string) {
 		clientProtocolVersionChan <- clientProtocolVersion
 	}()
 
-	dialer := net.Dialer{}
-	upstream, err := dialer.Dial("tcp", upstreamAddress)
-	if err != nil {
-		log.Println("dial err", err)
-		return
-	}
-	defer upstream.Close()
-
 	serverProtocolVersionBytes := betterio.ReadBytesUntilRune(upstream, '\n')
 
 	log.Println(conn.RemoteAddr().String(), "serverProtocolVersion", strings.TrimSpace(string(serverProtocolVersionBytes)))
+	if err := betterio.CheckReaderOpen(upstream); err != nil {
+		log.Println(conn.RemoteAddr().String(), "server", upstream.RemoteAddr().String(), "gone")
+		return
+	}
 
 	upstream.Write(serverProtocolVersionBytes)
 
 	<-clientProtocolVersionChan
+	if err := betterio.CheckReaderOpen(conn); err != nil {
+		log.Println(conn.RemoteAddr().String(), "client gone")
+		return
+	}
 
 	log.Println(conn.RemoteAddr().String(), "copying")
 	betterio.CopyBidirUntilCloseAndReturnBytesWritten(conn, upstream)
